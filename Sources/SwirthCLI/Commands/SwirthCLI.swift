f8c9548113604f86f3e46acfd212b5eb3c15b960
jake.oeding <jake.oeding@instructure.com>
2026-06-09 11:01:22 -0400

    update top level command name

--- Sources/SwirthCLI/Commands/SwirthCLI.swift
+++ Sources/SwirthCLI/Commands/SwirthCLI.swift
@@ -3,6 +3,7 @@ import ArgumentParser
 @main
 struct SwirthCLI: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
+        commandName: "swirth",
         abstract: "A minimal Forth implemented in Swift.",
         subcommands: [Interpret.self, Compile.self],
         defaultSubcommand: Interpret.self
